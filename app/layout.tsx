860688b7bbcfce5cefe8f326c8c77f64aeab7e91
weiwx <weiwx@mangdang.com>
2025-04-12 14:45:15 +0800

    remove verification for gsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,6 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  verification: {
-    google: 'your-google-verification-code',
-  },
 };
 
 export default function RootLayout({
